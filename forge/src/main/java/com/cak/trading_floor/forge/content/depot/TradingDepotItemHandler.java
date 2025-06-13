a7a108c37c54f5409dae03003807dd04063f7fe7
cake <cakeistastydev@gmail.com>
2025-06-13 17:08:00 +0100

    1.20.1-2.0.4 Tom's Simple Storage crash fix

--- forge/src/main/java/com/cak/trading_floor/forge/content/depot/TradingDepotItemHandler.java
+++ forge/src/main/java/com/cak/trading_floor/forge/content/depot/TradingDepotItemHandler.java
@@ -9,82 +9,83 @@ import net.minecraftforge.items.ItemHandlerHelper;
 import org.antlr.v4.runtime.misc.NotNull;
 
 public class TradingDepotItemHandler implements IItemHandler {
-    
+
     TradingDepotBehaviour behaviour;
-    
+
     public TradingDepotItemHandler(TradingDepotBehaviour behaviour) {
         this.behaviour = behaviour;
     }
-    
+
     @Override
     public int getSlots() {
         return 1 + behaviour.getResults().size();
     }
-    
+
     @Override
     public ItemStack getStackInSlot(int i) {
-        return i == 0 ? behaviour.getOfferStack() : behaviour.getResults().get(i - 1);
+        return i == 0 ? behaviour.getOfferStack() :
+            (i - 1 < behaviour.getResults().size() ? behaviour.getResults().get(i - 1) : ItemStack.EMPTY);
     }
-    
+
     @Override
     public ItemStack insertItem(int i, ItemStack arg, boolean bl) {
         if (i != 0) return arg;
-        
+
         if (!behaviour.getOfferStack().isEmpty() && !ItemHandlerHelper.canItemStacksStack(behaviour.getOfferStack(), arg))
             return arg;
-        
+
         ItemStack existingStack = behaviour.getOfferStack();
-        
+
         int oldCount = existingStack.getCount();
         int newCount = Math.min(existingStack.getMaxStackSize(), oldCount + arg.getCount());
         int added = newCount - oldCount;
         int remaining = arg.getCount() - added;
-        
+
         if (!bl) {
             behaviour.setOfferStack(arg.copyWithCount(newCount));
             if (newCount != oldCount)
                 behaviour.spinOfferOrSomething();
             behaviour.blockEntity.sendData();
         }
-        
+
         return arg.copyWithCount(remaining);
     }
-    
+
     @Override
     public ItemStack extractItem(int i, int j, boolean bl) {
         if (i == 0) return ItemStack.EMPTY;
-        
+
         int listIndex = i - 1;
         if (listIndex >= behaviour.getResults().size()) return ItemStack.EMPTY;
-        
+
         ItemStack currentStack = behaviour.getResults().get(listIndex);
-        
+
         int extractedCount = Math.min(currentStack.getCount(), j);
-        
+
         ItemStack resultStack = currentStack.copyWithCount(extractedCount);
         ItemStack remainderStack = currentStack.copyWithCount(currentStack.getCount() - extractedCount);
-        
+
         if (!bl) {
             this.behaviour.getResults().set(listIndex, remainderStack);
             this.behaviour.doPruneEmptyStacksNextTick();
             behaviour.blockEntity.sendData();
         }
-        
+
         return resultStack;
     }
-    
+
     @Override
     public int getSlotLimit(int i) {
         return 64;
     }
-    
+
     @Override
     public boolean isItemValid(int i, ItemStack arg) {
         return true;
     }
-    
+
     public ItemStack insertItem(TransportedItemStack transportedItemStack, Direction direction, boolean b) {
         return insertItem(0, transportedItemStack.stack, b);
     }
-    
+
 }
